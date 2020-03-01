5bdb348f54c636ba7f01594fa79d005c215527ab
AminurRouf <aminur.rouf@ncl.ac.uk>
2020-03-01 20:10:10 +0000

    added vue fundamental

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <fd-shell>
+    <fd-shell-header>
+      <shell-bar />
+    </fd-shell-header>
+    <fd-app>
+      <!-- inline style needed to work around a bug -->
+      <fd-app-navigation style="min-width: 250px;" orientation="vertical">
+        <fd-side-nav>
+          <fd-side-nav-list :items="navigationItems">
+            <template #item="item">
+              <fd-side-nav-item>
+                <fd-side-nav-link as-router-link :to="item.to">
+                  <fd-side-nav-icon :icon="item.icon" />
+                  {{ item.name }}
+                </fd-side-nav-link>
+              </fd-side-nav-item>
+            </template>
+          </fd-side-nav-list>
+        </fd-side-nav>
+      </fd-app-navigation>
+      <fd-app-main>
+        <router-view />
+      </fd-app-main>
+    </fd-app>
+  </fd-shell>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShellBar from "@/components/ShellBar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { ShellBar },
+  data() {
+    return {
+      navigationItems: [
+        { name: "Home", icon: "home", to: { name: "home" } },
+        {
+          name: "About",
+          icon: "sap-logo-shape",
+          to: { name: "about" }
+        }
+      ]
+    };
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/plugins/fundamental-vue.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import '../fundamental.scss'
+import FundamentalVue from 'fundamental-vue'
+import VueObserveVisibility from 'vue-observe-visibility'
+import VueVirtualScroller from 'vue-virtual-scroller'
+
+Vue.use(VueObserveVisibility)
+Vue.use(VueVirtualScroller)
+Vue.use(FundamentalVue)
